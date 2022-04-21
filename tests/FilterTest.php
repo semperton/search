5dd5712c0c118567bad3d0e5a9c5a49f4286d01f
Johannes Rabausch <jrabausch@absatzformat.de>
2022-04-21 10:00:27 +0200

    Added more tests

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use Semperton\Search\Condition;
 use Semperton\Search\Filter;
 
 final class FilterTest extends TestCase
@@ -31,6 +32,38 @@ final class FilterTest extends TestCase
 
 		foreach ($filter as $bool => $obj) {
 			$this->assertEquals(Filter::CONNECTION_OR, $bool);
+		}
+	}
+
+	public function testEqual(): void
+	{
+		$filter = Filter::create();
+		$filter->equal('number', 22);
+
+		foreach ($filter as $bool => $obj) {
+
+			$this->assertEquals(Filter::CONNECTION_AND, $bool);
+			$this->assertInstanceOf(Condition::class, $obj);
+
+			$this->assertEquals('number', $obj->getField());
+			$this->assertEquals(Condition::EQUAL, $obj->getOperator());
+			$this->assertEquals(22, $obj->getValue());
+		}
+	}
+
+	public function testGreater(): void
+	{
+		$filter = Filter::create();
+		$filter->greater('number', 22);
+
+		foreach ($filter as $bool => $obj) {
+
+			$this->assertEquals('and', $bool);
+			$this->assertInstanceOf(Condition::class, $obj);
+
+			$this->assertEquals('number', $obj->getField());
+			$this->assertEquals(Condition::GREATER_THAN, $obj->getOperator());
+			$this->assertEquals(22, $obj->getValue());
 		}
 	}
 }
