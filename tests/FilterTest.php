8b56d2a14cde1d94dd0c3d408fc5bcd417d14436
Johannes Rabausch <jrabausch@absatzformat.de>
2022-04-21 16:05:29 +0200

    Adjustments due to api change

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -8,17 +8,22 @@ use Semperton\Search\Filter;
 
 final class FilterTest extends TestCase
 {
+	protected function createFilter(): Filter
+	{
+		return new Filter();
+	}
+
 	public function testCreate(): void
 	{
-		$filter = Filter::create();
+		$filter = $this->createFilter();
 
 		$this->assertInstanceOf(Filter::class, $filter);
 	}
 
 	public function testAnd(): void
 	{
-		$filter = Filter::create();
-		$filter->and(Filter::create());
+		$filter = $this->createFilter();
+		$filter->and($this->createFilter());
 
 		foreach ($filter as $bool => $obj) {
 			$this->assertEquals(Filter::CONNECTION_AND, $bool);
@@ -27,8 +32,8 @@ final class FilterTest extends TestCase
 
 	public function testOr(): void
 	{
-		$filter = Filter::create();
-		$filter->or(Filter::create());
+		$filter = $this->createFilter();
+		$filter->or($this->createFilter());
 
 		foreach ($filter as $bool => $obj) {
 			$this->assertEquals(Filter::CONNECTION_OR, $bool);
@@ -37,7 +42,7 @@ final class FilterTest extends TestCase
 
 	public function testEqual(): void
 	{
-		$filter = Filter::create();
+		$filter = $this->createFilter();
 		$filter->equal('number', 22);
 
 		foreach ($filter as $bool => $obj) {
@@ -53,7 +58,7 @@ final class FilterTest extends TestCase
 
 	public function testGreater(): void
 	{
-		$filter = Filter::create();
+		$filter = $this->createFilter();
 		$filter->greater('number', 22);
 
 		foreach ($filter as $bool => $obj) {
